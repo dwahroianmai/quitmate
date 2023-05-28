5c90b7ca47de6bc49e515ac7176952877d7e3066
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-05-28 22:42:55 +0200

    add logs

--- client/src/components/healthAll.js
+++ client/src/components/healthAll.js
@@ -14,9 +14,10 @@ function Health() {
   );
 
   useEffect(() => {
-    axios
-      .get("https://quitmate-api.onrender.com/data")
-      .then((res) => setData(res.data["data"][0]));
+    axios.get("https://quitmate-api.onrender.com/data").then((res) => {
+      console.log(res);
+      setData(res.data["data"][0]);
+    });
   }, []);
 
   useEffect(() => {
@@ -28,12 +29,14 @@ function Health() {
   });
 
   function timePassed(time, usersTime) {
+    console.log(time, usersTime);
     let percent = time / 100;
     let usersPercent = usersTime / percent;
     return usersPercent;
   }
 
   function ifHundred(percent) {
+    console.log(percent);
     if (percent >= 100) {
       return 100;
     }
