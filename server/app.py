4d7e41dd532eb994aead0f4668bc759aa0ff2470
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-06-06 13:13:32 +0200

    prepare for redeploying

--- server/app.py
+++ server/app.py
@@ -25,7 +25,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 # cors
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
-app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SECURE"] = False
 
 Session(app)
 CORS(app, supports_credentials=True)
